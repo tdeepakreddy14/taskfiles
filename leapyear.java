2911e384cbc72950685532f57b0f7337df998182
tdeepakreddy14 <147715671+tdeepakreddy14@users.noreply.github.com>
2023-10-17 11:33:05 +0530

    Update leapyear.java

--- leapyear.java
+++ leapyear.java
@@ -1,33 +1,19 @@
-
 import java.util.Scanner;
-
 public class leapyear {
-
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.println("entee an year");
 		int year = sc.nextInt();
 		sc.close();
-		switch(year%4) {
-		case 0:
-			switch(year%100){
-				case 0:
-					switch(year%400) {
-					case 0:
-						System.out.println("leap year");
-						break;
-					default :
-						System.out.println(" not leap year");
-					}
-					break;
-			default :
+		int value = ((year%4==0 && year%100 != 0)|| year%400==0)? 0:1 ; // conditional (ternary) operator 
+		switch(value){
+			case 0:
 				System.out.println("leap year");
-			}
-			break;
-			
-		default :
-			System.out.println("not leap year");
-			break;
+				break;
+			default :
+				System.out.println("not leap year");
+				break;
 		}
 	}
 }
