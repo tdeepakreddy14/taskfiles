0dafcd1038cd114109f775a1066294ad171fc981
tdeepakreddy14 <147715671+tdeepakreddy14@users.noreply.github.com>
2023-10-18 17:28:31 +0530

    Update car.java

--- car.java
+++ car.java
@@ -1,62 +1,101 @@
 public class car {
-	private String colour;
-	private String brand;
-	private String engine;
-	private double bootspace;
-	private boolean ac; 
-	private boolean musicsystem;
-	private boolean navigation;
-	private double price;
-	private String cartype;
-	public car(String colour,String brand,String engine,double bootspace,boolean ac,boolean musicsystem,boolean navigation,double price,String cartype) {
+	private String colour,brand,engine,cartype;
+	private double bootspace,price;
+	private boolean ac,musicsystem,navigation;
+
+	public void setcolour(String colour) {
 		this.colour = colour;
-		this.brand = brand;
-		this.engine = engine;
-		this.bootspace = bootspace;
+	}
+	public void setbrand(String brand) {
+		this.brand=brand;
+	}
+	public void setengine(String engine) {
+		this.engine=engine;
+	}
+	public void setbootspace(double bootspace) {
+		this.bootspace=bootspace;
+	}
+	public void setac(boolean ac) {
 		this.ac = ac;
-		this.musicsystem = musicsystem;
-		this.navigation = navigation;
-		this.price = price;
-		this.cartype = cartype; 
 	}
-	public void display_car_info() {
-		System.out.println("colour: "+colour);
-		System.out.println("brand: "+brand);
-		System.out.println("engine: "+engine);
-		System.out.println("boot space: "+bootspace);
-		System.out.println("ac: "+ac);
-		System.out.println("musicsystem: "+musicsystem);
-		System.out.println("navigation: "+navigation);
-		System.out.println("price: "+price);
-		System.out.println("car type: "+cartype);
+	public void setmusicsystem(boolean musicsystem) {
+		this.musicsystem=musicsystem;
 	}
-	public void startthecar() {
-		System.out.println("starting the car ");
+	public void setnavigation (boolean navigation) {
+		this.navigation=navigation;
 	}
-	public void movethecar() {
-		System.out.println("moving the car");
+	public void setprice(double price) {
+		this.price=price;
 	}
-	public void playmusic() {
+	public void setcartype(String cartype) {
+		this.cartype=cartype;
+	}
+	
+	public String getcolour() {
+		return colour;
+	}
+	public String getbrand() {
+		return brand;
+	}
+	public String getengine() {
+		return engine;
+	}
+	public double getbootspace() {
+		return bootspace;
+	}
+	public boolean getac() {
+		return ac;
+	}
+	public boolean getmusicsystem() {
+		return musicsystem;
+	}
+	public boolean getnavigation () {
+		return navigation;
+	}
+	public double getprice() {
+		return price;
+	}
+	public String getcartype() {
+		return cartype;
+	}
+	
+	public String toString(){
+		return colour+" colour, "+brand+", "+engine+", "+bootspace+" lt's, ac : "+ac+", musicsystem : "+musicsystem+", navigation : "+navigation+", "+price+" /Rs, "+cartype;
+	}
+	public String startengine() {
+		String start = "starting the car ";
+		return start ;
+	}
+	public String movethecar() {
+		String move = "moving the car";
+		return move;
+	}
+	public String playmusic() {
+		String result;
 		if(musicsystem) {
-			System.out.println("playing music");
+			result = "playing music";
 		}
 		else{
-			System.out.println("no music system");
+			result = "no music system";
 		}
+	return result;
 	}
-	public void applybreak() {
-		System.out.println("applying the break");
+	public String applybreak() {
+		String breake = "applying the break";
+		return breake;
 	}
 	public static void main(String[] args) {
-		car mycar = new car("red","virtus","bs6",400,true,true,false,1500000,"sedan");
-		System.out.println("CAR DETAILS - ");
-		System.out.println("");
-		mycar.display_car_info();
-		System.out.println(' ');
-		System.out.println("CAR FUNCTIONALITYS - ");
-		mycar.startthecar();
-		mycar.movethecar();
-		mycar.playmusic();
-		mycar.applybreak();
+		car mycar = new car();
+		mycar.setcolour("red");
+		mycar.setbrand("virtus");
+		mycar.setengine("bs6");
+		mycar.setbootspace(400);
+		mycar.setac(true);
+		mycar.setmusicsystem(true);
+		mycar.setnavigation(false);
+		mycar.setprice(1500000);
+		mycar.setcartype("sedan");
+		System.out.println("car details : "+mycar+"\n\nCAR FUNCTIONALITYS -  " + mycar.startengine() +", "+ mycar.movethecar() +", "+ mycar.playmusic() +", "+ mycar.applybreak());
+		
 	}
 }
