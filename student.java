f4a5bcf47e847facdb36966f7f6fe1abfa46db24
tdeepakreddy14 <147715671+tdeepakreddy14@users.noreply.github.com>
2023-10-18 17:29:52 +0530

    Update student.java

--- student.java
+++ student.java
@@ -1,23 +1,28 @@
-class student{
+public class student{
 	String studName;
 	int studRollno;
-	public student(String studName , int studRollno) {
+	public void setName(String studName) {
 		this.studName =studName;
+	}
+	public void setRollno(int studRollno) {
 		this.studRollno=studRollno;
 	}
-	public String getName() {
+	String getName() {
 		return studName;
 	}
-	
 	public int getRollno() {
 		return studRollno;
 	}
 	public static void main(String[] args) {
-		student s1 = new student("deepak",21);
-		student s2 = new student("charan",20);
-		student s3 = new student("yashu",22);
+		student s1 = new student();
+		s1.setName("deepak");
+		s1.setRollno(222);
+		
+		student s2 = new student();
+		s2.setName("yashu");
+		s2.setRollno(211);
+
 		System.out.println("student1 name: "+s1.getName()+", student1 age: "+ s1.getRollno());
 		System.out.println("student2 name: "+s2.getName()+", student2 age: "+ s2.getRollno());
-		System.out.println("student3 name: "+s3.getName()+", student3 age: "+ s3.getRollno());
 	}
 }
