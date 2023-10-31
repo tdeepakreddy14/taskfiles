b15718cbe05d32703eb5c037ce9fcd5fab5fd3e5
tdeepakreddy14 <147715671+tdeepakreddy14@users.noreply.github.com>
2023-10-31 10:38:37 +0530

    Update arrDoubleTriple.java

--- task7/arrDoubleTriple.java
+++ task7/arrDoubleTriple.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 import java.util.*;
 public class arrDoubleTriple{
 	public static void main(String[] args) {
-		int[] inputArray = {1,2,3,4,5,6};
+		Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Array Size:");
+        int n=sc.nextInt();
+        int[] inputArray = new int[n];
+        System.out.println("Enter Array elements: ");
+        for(int i=0; i<n; i++)
+        {
+            inputArray[i]=sc.nextInt();
+        }
 		int[] modifiedarray = new int[inputArray.length];
 		
 		for(int i=0;i<inputArray.length;i++) {
